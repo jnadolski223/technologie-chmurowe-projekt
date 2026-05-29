4643218463f0f64a9e67e4674497b61a9f746903
Jakub Nadolski <j.nadolski223@gmail.com>
2026-05-29 14:34:48 +0200

    Added Dockerfile and dockerignore for frontend, backend and worker. Small changes

--- backend/src/main/java/pl/edu/ug/eventmanagerbackend/config/ApiPaths.java
+++ backend/src/main/java/pl/edu/ug/eventmanagerbackend/config/ApiPaths.java
@@ -4,7 +4,7 @@ public class ApiPaths {
 
     private ApiPaths() {}
 
-    public static final String BASE = "/api/v1";
+    public static final String BASE = "";
     public static final String USERS = BASE + "/users";
     public static final String EVENTS = BASE + "/events";
     public static final String BOOKINGS = BASE + "/bookings";
--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,4 +1,4 @@
 export const config = {
-  apiBaseUrl: 'http://localhost:8080/api/v1',
+  apiBaseUrl: '/api',
   userStorageKey: 'user'
 };
